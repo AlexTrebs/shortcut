cd9649604cefa1849fb9cd48f28f513270a7124b
root <root@Laptop.>
2025-04-12 19:27:00 +0100

    feature/create-ci-and-fix-errors * Created CI file * Added build check * Added testing check * Added linting check * Added running and api check * Fixed linting and test errors

--- src/templates/mod.rs
+++ src/templates/mod.rs
@@ -1,6 +1,6 @@
 use crate::{TERA, macros::renderable::Renderable};
 
-use axum::{response::Redirect, routing::get, Router};
+use axum::{http::StatusCode, response::{IntoResponse, Redirect}, routing::get, Router};
 use pages::{CreatePageTemplate, SearchPageTemplate};
 
 pub mod components;
@@ -10,9 +10,15 @@ async fn redirect_to_search() -> Redirect {
   Redirect::to("/search")
 }
 
+async fn health_check() -> impl IntoResponse {
+  // Return HTTP 200 OK to signify the application is healthy
+  StatusCode::OK
+}
+
 pub fn create_ui_routes() -> Router {
   Router::new()
-  .route("/", get(redirect_to_search))
-  .route("/search", get(SearchPageTemplate{}.get_html(TERA.read().unwrap().clone())))
-  .route("/create", get(CreatePageTemplate{}.get_html(TERA.read().unwrap().clone())))
+    .route("/", get(redirect_to_search))
+    .route("/healthcheck", get(health_check))
+    .route("/search", get(SearchPageTemplate{}.get_html(TERA.read().unwrap().clone())))
+    .route("/create", get(CreatePageTemplate{}.get_html(TERA.read().unwrap().clone())))
 }
